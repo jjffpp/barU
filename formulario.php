c68035ad19242a7c05ab026ead7ce64901ff63ad
jjffpp <juanfranciscopose@gmail.com>
2018-05-26 16:46:05 -0300

    agrego archivo de validaciones de campos del formulario
    en esta version implemento la funcionalidad para validar todos los campos del formulario.

--- formulario.php
+++ formulario.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en" dir="ltr">
 <head>
+   <meta charset="utf-8">
    <link rel="stylesheet" type="text/css" href="style.css">
+   <script src="validarFormulario.js" language="javascript" type="text/javascript"></script>
    <title>Crear viaje</title>
-
 </head>
 <body>
-  <div class="f1">
-  <form action="formularioAltaViaje.php" method="post">
+  <form action="formularioAltaViaje.php" method="post" id="formulario">
      <div class="container">
        <h1>Creación de viaje</h1>
        <p>Por favor, complete todos los campos para crear un nuevo viaje</p>
        <hr>
-       <label for:"costo"><b>Costo<b><br></label>
-       <input type="text" name="costo"/><br/>
-       <label for:"duracion"><b>Duracion (redondeo) <b><br></label>
-       <input type="text" name="duracion"/><br/>
+       <label for:"costo" ><b>Costo<b><br></label>
+       <input type="text" id="costo" name="costo"/><br/>
+       <label for:"duracion"><b>Duracion (en hs) <b><br></label>
+       <input type="text" id="duracion" name="duracion"/><br/>
        <label for:"tipo"><b>Tipo viaje<b><br></label>
-       <select name="tipo">
+       <select name="tipo" id="tipo">
          <option value="" selected="selected"></option>
          <option value="semanal">semanal</option>
          <option value="diario">diario</option>
          <option value="ocacional">ocacional</option>
        </select><br/>
        <label for:"origen"><b>Lugar de partida<b><br></label>
-       <select name="origen">
+       <select name="origen" id="origen">
          <option value="" selected="selected">Elegi un origen</option>
          <?php
          require 'conexion.php';
@@ -37,24 +36,26 @@
          }
          echo "</select><br/>";
          echo "<label for:\"destino\"><b>Lugar de destino<b><br></label>
-                <select name=\"destino\">
+                <select name=\"destino\" id=\"destino\">
                 <option value=\"\" selected=\"selected\">Elegi un destino</option>";
                 mysqli_data_seek($resulQuery,0);
                 while($row = mysqli_fetch_assoc($resulQuery)) {
                   echo "<option value=\"" . $row["idlocalidades"] . "\">" . $row["nombre"] . "</option>";
                 }
-        echo "</select><br/>";
+         echo "</select><br/>";
         ?>
         <label for:"fecha"><b>Fecha de partida<b><br></label>
-        <input type="date" name="fecha"/><br/>
+        <input type="date" id="fecha" name="fecha"/><br/>
         <label for:"hora"><b>Hora de partida<b><br></label>
-        <input type="time" name="hora"/><br/>
+        <input type="time" id="hora" name="hora"/><br/>
         <div class="botones">
-          <button type="button" class="crearViaje">Crear viaje</button>
+          <button type="button" onclick="validarCampos()" class="crearViaje">Crear Viaje</button>
+          <button type="submit" id="send" style="display:none;"></button>
           <button type="button" class="cancelar">Cancelar</button>
         </div>
       </div>
     </form>
-    </div>
+    <script type="text/javascript" src="formularioCorrecto()">
+    </script>
   </body>
   </html>
--- formularioAltaViaje.php
+++ formularioAltaViaje.php
@@ -9,58 +9,13 @@ $destino = $_POST["destino"];
 $fecha = $_POST["fecha"];
 $hora = $_POST["hora"];
 
-if(!validoNumericos($costo, $duracion, $origen, $destino)){
-/*echo '<script language="javascript">alert("';
-echo "campos en blanco";
-echo '");</script>';
-*/
+//inserto en la base de datos
+$combinedDT = date('Y-m-d H:i:s', strtotime("$fecha $hora"));
+$conn= new conexion();
+//la consulta esta con el vehiculo truncado en 4
+$consulta= "INSERT INTO `viajes`(`fechaYHora`, `tipo`, `duracion`, `costo`, `localidad_origen`, `localidad_destino`, `idvehiculo`) VALUES
+('$combinedDT','$tipo','$duracion','$costo','$origen','$destino',4)";
+$conn->consultarABD($consulta);
 header("location: formulario.php");
-}else{
-	//validacion fecha
-	$fecha_actual = date('Y-m-d');
-	if ($fecha_actual > $fecha){
-			header("location: formulario.php");
-	}else{
-		//valida hora
-			if (empty($hora)){
-				header("location: formulario.php");
-			}else{
-				//valida tipo
-				if (empty($tipo)){
-					header("location: formulario.php");
-				}else{
-					//inserto en la base de datos
-					$combinedDT = date('Y-m-d H:i:s', strtotime("$fecha $hora"));
-					$conn= new conexion();
-					//la consulta esta con el vehiculo truncado en 4
-					$consulta= "INSERT INTO `viajes`(`fechaYHora`, `tipo`, `duracion`, `costo`, `localidad_origen`, `localidad_destino`, `idvehiculo`) VALUES
-					('$combinedDT','$tipo','$duracion','$costo','$origen','$destino',4)";
-					$conn->consultarABD($consulta);
-					header("location: formulario.php");
-			}
-		}
-	}
-}
-
-//validacion blancos y numericos (falta vehiculo aca!)
-function validoNumericos ($c, $dur, $o, $des ){
-	if (!is_numeric($c)){
-		return false;
-	}else{
-		if (!is_numeric($dur)){
-			return false;
-		}else{
-			if (!is_numeric($o)){
-				return false;
-			}else{
-				if (!is_numeric($des)){
-					return false;
-				}else{
-					return true;
-				}
-			}
-		}
-	}
-}
 
 ?>
