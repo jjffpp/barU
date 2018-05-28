8d695794676b32d270fedede7f4d481b68733308
Malalujuria <andrees2488@gmail.com>
2018-05-27 21:16:45 -0300

    Se agrega la consulta
    El parametro enviado por Ajax es utilizado en esta ocacion para pasárselo a la consulta de la BD.
    La consulta devuelve una fila con los datos compeltos del viaje con el vehículo asociado.

--- mostrar_detalle_tarjeta.php
+++ mostrar_detalle_tarjeta.php
@@ -1,12 +1,16 @@
 <?php
 
   include_once "detalle_tarjeta.php";
+  include_once "../consultas_bd.php";
 
   //idviaje para realizar la consulta completa
-  $para = $_GET['param1'];
+  $idviaje = $_POST['param1'];
 
-  //Acá se encontrará la consulta a la BD de idViaje con Vehiculo
-  
+  //consulta a la BD con idviaje
+  $resultado_consullta = consulta_viaje_vehiculo($idviaje);
+  //fetch_assoc en una sola fila
+  $valor = $resultado_consullta->fetch_assoc();
+
   //en caso que el usuario esté loggeado se accede
   if(isset($_COOKIE['user'])){
     /*
@@ -14,7 +18,11 @@
     disponible(1/0) -> 1 el viaje es acivo para el usuario; 0 el viaje no es activo.
     La disponibilidad es establece por una consulta a la BD
     */
-    echo impresion_detalle_viaje('La Plata','Ensenada','Andres','1234','1234','aaaa',$para,4,10,1);
+    echo impresion_detalle_viaje($valor['idviajes'],$valor['fechaYHora'],$valor['tipo'],
+      $valor['duracion'],$valor['costo'],$valor['localidad_origen'],
+      $valor['localidad_destino'],$valor['idvehiculo'],
+      $valor['capacidad'],$valor['modelo'],$valor['descripcion'],$valor['owner'],1);
+
   }
 
 ?>
