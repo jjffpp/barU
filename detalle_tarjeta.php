0180b7dd4a2610e2345558717c9f5c83e3d98637
Malalujuria <andrees2488@gmail.com>
2018-05-27 21:10:37 -0300

    Modificacion en parametro, asientos y fecha
    Se realizan cambios en los parametros, ahora se reciben los valores completos de la consulta.
    La fecha de finalizacion es la duracion mas la fecha de inicio del viaje.
    Los asientos ocupados derivan de la capacidad menos los asientos disponibles.
    Se agrega el precio por persona, el cual es el precio final para el usuario que quiera sumarse.

--- detalle_tarjeta.php
+++ detalle_tarjeta.php
@@ -1,11 +1,19 @@
 <?php
 
-function impresion_detalle_viaje($origen,$destino,$chofer,$fechaInicio
-                          ,$fechaFinalizacion,$tipo,$asientosDisponibles
-                          ,$capacidad,$precioPersona,$disponible){
+function impresion_detalle_viaje($idviaje,$fechaYHora,$tipo,$duracion,$costo
+                          ,$origen,$destino,$idvehiculo,$capacidad,$modelo
+                          ,$descripcion_vehiculo, $dueño,$disponible){
   /*
   Funcion que imprime el detalle completo de cada viaje.
   */
+  //formateo el tiempo para poder sumarle la duracion y obtener la fecha de finalizacion
+  $fechaFinalizacion = new DateTime($fechaYHora);
+  $fechaFinalizacion->add(new DateInterval('PT'.$duracion.'H'));
+  //$asientosDisponibles aun no se encuentra en la BD
+  $asientosDisponibles = 1;
+  $asientos_ocupados = $capacidad - $asientosDisponibles;
+  $precio_persona = $costo / $asientos_ocupados;
+  
   $salida = "
   <div class='container'>
     <article id='main-col'>
@@ -14,24 +22,32 @@ function impresion_detalle_viaje($origen,$destino,$chofer,$fechaInicio
         <li>
           <div class='card' style='width: 18rem;'>
             <div class='card-body'>
+              <h5 class='card-title'>Numero de viaje: </h5>
+              <h6 class='card-subtitle mb-2 text-muted'>".$idviaje."</h6>
               <h5 class='card-title'>Origen: </h5>
               <h6 class='card-subtitle mb-2 text-muted'>".$origen."</h6>
               <h5 class='card-title'>Destino</h5>
               <h6 class='card-subtitle mb-2 text-muted'>".$destino."</h6>
               <h5 class='card-title'>Chofer</h5>
-              <h6 class='card-subtitle mb-2 text-muted'>".$chofer."</h6>
+              <h6 class='card-subtitle mb-2 text-muted'>".$dueño."</h6>
               <h5 class='card-title'>Fecha de Inicio</h5>
-              <h6 class='card-subtitle mb-2 text-muted'>".$fechaInicio."</h6>
+              <h6 class='card-subtitle mb-2 text-muted'>".$fechaYHora."</h6>
               <h5 class='card-title'>Fecha de Finalizacion</h5>
-              <h6 class='card-subtitle mb-2 text-muted'>".$fechaFinalizacion."</h6>
+              <h6 class='card-subtitle mb-2 text-muted'>".$fechaFinalizacion->format('Y-m-d H:i:s')."</h6>
+              <h5 class='card-title'>Duracion del Viaje</h5>
+              <h6 class='card-subtitle mb-2 text-muted'>".$duracion."</h6>
               <h5 class='card-title'>Tipo de Viaje</h5>
               <h6 class='card-subtitle mb-2 text-muted'>".$tipo."</h6>
+              <h5 class='card-title'>Modelo del Vehiculo</h5>
+              <h6 class='card-subtitle mb-2 text-muted'>".$modelo."</h6>
+              <h5 class='card-title'>Descripcion del Vehiculo</h5>
+              <h6 class='card-subtitle mb-2 text-muted'>".$descripcion_vehiculo."</h6>
               <h5 class='card-title'>Asientos Disponibles</h5>
               <h6 class='card-subtitle mb-2 text-muted'>".$asientosDisponibles."</h6>
               <h5 class='card-title'>Capacidad del Vehiculo</h5>
               <h6 class='card-subtitle mb-2 text-muted'>".$capacidad."</h6>
               <h5 class='card-title'>Precio Por Persona</h5>
-              <h6 class='card-subtitle mb-2 text-muted'>".$precioPersona."</h6>
+              <h6 class='card-subtitle mb-2 text-muted'>".round($precio_persona)."</h6>
               ";
   /*
   Si el viaje es activo para el usuario se visualiza el boton para poder darse de baja
