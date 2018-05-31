f87523a3f6c3457d2bbfbe7b1d27bcb97db44548
Ezequiel Masciarelli <ezequielmasciarelli@outlook.com>
2018-05-31 15:38:51 -0300

    Se modifica la navbar
    Se actualiza navBar y agrega index user no idenficiado

--- navbar.php
+++ navbar.php
@@ -1,5 +1,41 @@
 <?php
 
+function imprimir_narvar_visitante(){
+
+$salida = "
+<div class='example3'>
+  <nav class='navbar navbar-inverse navbar-fixed-top'>
+    <div class='container'>
+      <div class='navbar-header'>
+        <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar3'>
+          <span class='sr-only'>Toggle navigation</span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+        </button>
+        <a class='navbar-brand' href='index_user_identificado.php'><img src='aventonnegro.png'></a>
+      </div>
+      <div class='navbar-collapse collapse'>
+        <ul id='ulnav' class='nav navbar-nav navbar-right'>
+          <li><a href='buscarviaje'><span class='glyphicon glyphicon-search'></span> Buscar Viajes</a></li>
+          <li class='dropdown'>
+            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'><span class='glyphicon glyphicon-user'></span> Visitante <span class='caret'></span></a>
+            <ul class='dropdown-menu' role='menu'>
+              <li><a href='#'>Iniciar Sesion</a></li>
+              <li><a href='#'>Registrarse</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--/.nav-collapse -->
+    <!--/.container-fluid -->
+  </nav>
+</div>
+";
+return $salida;
+}
+
 function imprimir_narvar(){
 
 $salida = "
@@ -19,18 +55,13 @@ $salida = "
         <ul id='ulnav' class='nav navbar-nav navbar-right'>
           <li><a href='index_mis_viajes.php'><span class='glyphicon glyphicon-briefcase'></span> Mis Viajes</a></li>
           <li><a href='#' onclick='comprobarCondiciones()'><span class='glyphicon glyphicon-map-marker'></span> Crear Viaje</a></li>
-          <li><a href='buscarviaje'><span class='glyphicon glyphicon-search'></span> Buscar Viajes</a></li>
-          <li><a href='registrarvehiculo'><span class='glyphicon glyphicon-road'></span> Registrar Vehiculo</a></li>
+          <li><a href='#'><span class='glyphicon glyphicon-search'></span> Buscar Viajes</a></li>
+          <li><a href='#'><span class='glyphicon glyphicon-road'></span> Registrar Vehiculo</a></li>
           <li class='dropdown'>
             <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'><span class='glyphicon glyphicon-user'></span> Mi Usuario <span class='caret'></span></a>
             <ul class='dropdown-menu' role='menu'>
-              <li><a href='#'>Action</a></li>
-              <li><a href='#'>Another action</a></li>
-              <li><a href='#'>Something else here</a></li>
-              <li class='divider'></li>
-              <li class='dropdown-header'>Nav header</li>
-              <li><a href='#'>Separated link</a></li>
-              <li><a href='#'>One more separated link</a></li>
+              <li><a href='#'>Ver Perfil</a></li>
+              <li><a href='#'>Cerrar Sesion</a></li>
             </ul>
           </li>
         </ul>
