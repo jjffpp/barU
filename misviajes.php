bb274d7c793e1b7ad25a8f7ba7a6f9229936467a
Andres Gismondi <andres.gismondi@despegar.com>
2018-06-28 22:23:00 -0300

    pequeños errores solucionados

--- misviajes.php
+++ misviajes.php
@@ -1,11 +1,11 @@
 <?php
-session_start();
+//session_start();
 include_once "tarjeta.php";
 include_once "consultas_bd.php";
 
 //CONSULTA A LA BD
 $resultado = consultarMisViajes($_SESSION["idUsuario"]);//esta es a modo de prueba
-var_dump($resultado);
+
 /*
 idviaje,origen,destino,chofer,fechaInicio,fechaFinalizacion,estado,tipo,disponible
 disponible(1/0) -> si hay un usuario loggeado; 0 si no hay un usuario loggeado.
--- usuarioEstaSumadoAlViaje.php
+++ usuarioEstaSumadoAlViaje.php
@@ -2,14 +2,14 @@
 include "consultas_bd.php";
 function usuarioEstaSumadoAlViaje($idViaje,$idUsuario)
 {
-  echo $idUsuario;
+
   $cont = 0;
 $conn= usuarioEnSemana($idViaje,$idUsuario);
 while ($fila = $conn->fetch_assoc()) {
   //print_r($fila);
   $cont++;
 }
-echo $cont;
+
 if ($cont > 0) {
 
         return true;
