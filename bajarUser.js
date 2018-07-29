82a7d98a5809ced83f72e21b791c90d10d62782d
EzeXP <37245169+EzeXP@users.noreply.github.com>
2018-07-29 19:03:17 -0300

    Agrega la capacidad de eliminar un viaje
    Ahora se pueden eliminar los viajes creados siendo el chofer del viaje

--- bajarUser.js
+++ bajarUser.js
@@ -16,5 +16,17 @@ function bajarseDelViaje(idViaje) {
 
 function eliminarViaje(idViaje)
 {
-  bootbox.confirm("Realmente quiere eliminar el viaje?", function(result){ console.log('This was logged in the callback: ' + result); });
+  bootbox.confirm("Realmente desea eliminar el viaje? Esta operacion bajara a todos los pasajeros", function(result){
+    if(result==true){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("user borrado del viaje");
+            location.reload();
+        }
+    };
+    xmlhttp.open("POST", "eliminarViaje.php", true);
+    xmlhttp.send(idViaje);
+    }
+   });
 }
