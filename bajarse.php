d12865fb7c47087e080e44d92aaba047f22f4e5c
Ezequiel Masciarelli <ezequiel.masciarelli@despegar.com>
2018-08-02 11:58:18 -0300

    se borra todo por problemas tecnicos

--- bajarse.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  require 'conexion.php';
-  $idUser = $_SESSION["idUsuario"];
-  $idViaje = file_get_contents('php://input');
-  $conn= new conexion();
-  $consulta1 = "DELETE FROM usuarios_has_viajes WHERE usuarios_idUsuario='$idUser' AND viajes_idviajes='$idViaje'";
-  $resulQuery1 = $conn->consultarABD($consulta1);
- ?>
