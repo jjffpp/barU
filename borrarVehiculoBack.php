065720532289e0608efaa849efe050f4219b6d28
jjffpp <juanfranciscopose@gmail.com>
2018-08-03 14:39:33 -0300

    back borrar vehiculo
    elimina tupla de la base de datos
    falta validacion que el vehiculo no participe de ningun viaje

--- borrarVehiculoBack.php
+++ borrarVehiculoBack.php
@@ -1,4 +1,9 @@
 <?php
-echo "lleue";
-
- ?>
+include "conexion.php";
+$id = $_GET["idvehiculo"];
+$conn= new conexion();
+$consulta1 = "DELETE FROM vehiculo
+              WHERE idvehiculo = '$id'";
+$resultado1 = $conn->consultarABD($consulta1);
+header("location: borrarVehiculoFront.php");  
+?>
